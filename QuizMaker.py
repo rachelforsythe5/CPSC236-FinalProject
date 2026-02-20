f6ddcd4d82bbe75b6cce64ce0b5edc1b79a5b9b0
rachelforsythe5 <rachelforsythe5@gmail.com>
2026-02-20 13:10:57 -0500

    Initial commit

---
+++ QuizMaker.py
@@ -0,0 +1,29 @@
+# CPSC 236 Final Project
+# Group Members: Brady Crago, Rachel Forsythe, Lukas Holsopple
+# Team Leader: Rachel Forsythe
+
+## QUIZ MAKER ##
+
+# Import text bank file
+
+# Get user input
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+id = int(input("Enter your ID (Ex. A01234): "))
+
+# Validate ID
+
+
+# Randomly pick 10 unique questions
+
+
+# Display one question at a time
+
+
+# Invalid answers
+
+
+# Create a text file
+
+
+# Exit program prompt
